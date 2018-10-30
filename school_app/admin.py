f8494b3ade0c75976afd057e7adbabf03b68a61a
Pritom <pritom.saan@gmail.com>
2018-10-30 15:56:25 +0600

    Created Models

--- school_app/admin.py
+++ school_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from school_app.models import SchoolData, CourseData, LectureData
 
 # Register your models here.
+admin.site.register(SchoolData)
+admin.site.register(CourseData)
+admin.site.register(LectureData)
