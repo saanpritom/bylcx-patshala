bdf55a9ea298caba494a66b5b918fa0f77824620
Pritom <pritom.saan@gmail.com>
2018-10-30 11:35:36 +0600

    Template Setup

--- school_app/urls.py
+++ school_app/urls.py
@@ -3,4 +3,5 @@ from school_app import views
 
 urlpatterns = [
     path('', views.index, name='home_page'),
+    path('contact', views.ContactView.as_view(), name='contact_page')
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.views.generic import View, TemplateView
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'userwebsite/home-page-view.html')
+
+
+class ContactView(TemplateView):
+    template_name = 'userwebsite/contact-view.html'
