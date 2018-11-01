62913fef6960bae79e63de85de3c5e00bac9af6f
Pritom <pritom.saan@gmail.com>
2018-11-01 17:09:32 +0600

    Fixed search page

--- school_app/templatetags/video_controll.py
+++ school_app/templatetags/video_controll.py
@@ -15,31 +15,3 @@ def previousmodal(current_id_args):
     """Decrement one value"""
     current_id_args = current_id_args - 1
     return current_id_args
-
-
-@register.filter
-def nextelement(course_id, lecture_id):
-    lecture_id_list = list(LectureData.objects.filter(course=course_id))
-    return_lect = 1
-
-    for i in range(len(lecture_id_list)):
-        if i == lecture_id:
-            return_lect = lecture_id_list[i]
-            break
-
-    return return_lect
-    ##return lecture_id_list
-
-
-@register.filter
-def lecture_description_id(current_id_args):
-    current_id_args = str(current_id_args)
-    current_id_args = 'description' + current_id_args
-    return current_id_args
-
-
-@register.filter
-def lecture_description_button(current_id_args):
-    current_id_args = str(current_id_args)
-    current_id_args = 'button' + current_id_args
-    return current_id_args
