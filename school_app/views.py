56ccc8cc62f6258951205f33de9c6a383206b0f6
Pritom <pritom.saan@gmail.com>
2018-11-14 15:28:02 +0600

    Improved Contact Page Email Send

--- school_app/views.py
+++ school_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.db.models import Q
 from django.views.generic import View, TemplateView, ListView, DetailView
